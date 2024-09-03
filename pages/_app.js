c1849db25308ab586e56c45eae7ccc0da74dc4bb
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-09-04 01:35:48 +0330

    Feat:create layout(header and footer) with styles & global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+//styles
 import '../styles/globals.css'
-import '../styles/fonts.css'
+/* import '../styles/fonts.css' */
+//components
+import Layout from '../components/layout/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@
 export default function Home() {
   return (
     <div >
-      <h1>dd</h1>
     </div>
   )
 }
