3c5d00379f365d8fb1b37eb41cf0541f2fc32515
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-09-15 13:56:46 +0330

    Feat & Edit:create ctegory component with styles and edit categories components

--- components/module/Categories.js
+++ components/module/Categories.js
@@ -5,13 +5,21 @@ import Category from './Category'
 import styles from "./Categories.module.css"
 
 function Categories() {
-    const catefories = [
-        {id:1 , src:"./images/Breakfast.png" , alt:"Breakfast"},
-        {id:2 , src:"./images/Vegan.png" , alt:"Vegan"},
-        {id:3 , src:"./images/Meat.png" , alt:"Meat"},
-        {id:4 , src:"./images/Dessert.png" , alt:"Dessert"},
-        {id:5 , src:"./images/Lunch.png" , alt:"Lunch"},
-        {id:6 , src:"./images/Chocolate.png" , alt:"Chocolate"},
+    const categories = [
+        {id:1, src:"/images/Breakfast.png", alt:"Breakfast", name:"Breakfast"},
+        {id:2, src:"/images/Vegan.png", alt:"Vegan", name:"Vegan"},
+        {id:3, src:"/images/Meat.png", alt:"Meat", name:"Meat", },
+        {id:4, src:"/images/Dessert.png", alt:"Dessert", name:"Dessert"},
+        {id:5, src:"/images/Lunch.png", alt:"Lunch", name:"Lunch"},
+        {id:6, src:"/images/Chocolate.png", alt:"Chocolate", name:"Chocolot"},
+    ]
+    const backgroundColor = [
+      "linear-gradient(180deg, rgba(112, 130, 70, 0) 0%, rgba(112, 130, 70, 0.1) 100%);",
+      "linear-gradient(180deg, rgba(108, 198, 63, 0) 0%, rgba(108, 198, 63, 0.1) 100%);",
+      "linear-gradient(180deg, rgba(204, 38, 27, 0) 0%, rgba(204, 38, 27, 0.1) 100%);",
+      "linear-gradient(180deg, rgba(240, 158, 0, 0) 0%, rgba(240, 158, 0, 0.1) 100%);",
+      "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.05) 100%);",
+      "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.05) 100%);"
     ]
   return (
     <div className={styles.container}>
@@ -19,7 +27,9 @@ function Categories() {
             <h3>Categories</h3>
             <Link href="/categories">View All Categories</Link>
         </div>
-        {catefories.map(category => <Category key={category.id} data={category}/>)}
+        <div className={styles.categories}>
+          {categories.map((category,index) => <Category key={category.id} data={category}  style ={{backgroundColor:backgroundColor[index]}}/>)}   
+        </div>
     </div>
   )
 }
--- components/module/Category.js
+++ components/module/Category.js
@@ -1,9 +1,15 @@
+import Image from "next/image"
 //Styles
 import styles from "./Category.module.css"
 
-function Category() {
+function Category({data}) {
+  const {src, alt, name} = data
+
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <Image src={src} alt={alt} width={200} height={500}/>
+      <h3>{name}</h3>
+    </div>
   )
 }
 
