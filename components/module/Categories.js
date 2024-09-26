0a6ded7d242ea4255dfdf6e58627fe242fb9b75c
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-09-26 22:39:37 +0330

    Feat & Edit:create ctegory component with styles and edit styles

--- components/module/Categories.js
+++ components/module/Categories.js
@@ -13,14 +13,6 @@ function Categories() {
         {id:5, src:"/images/Lunch.png", alt:"Lunch", name:"Lunch"},
         {id:6, src:"/images/Chocolate.png", alt:"Chocolate", name:"Chocolot"},
     ]
-    const backgroundColor = [
-      "linear-gradient(180deg, rgba(112, 130, 70, 0) 0%, rgba(112, 130, 70, 0.1) 100%);",
-      "linear-gradient(180deg, rgba(108, 198, 63, 0) 0%, rgba(108, 198, 63, 0.1) 100%);",
-      "linear-gradient(180deg, rgba(204, 38, 27, 0) 0%, rgba(204, 38, 27, 0.1) 100%);",
-      "linear-gradient(180deg, rgba(240, 158, 0, 0) 0%, rgba(240, 158, 0, 0.1) 100%);",
-      "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.05) 100%);",
-      "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.05) 100%);"
-    ]
   return (
     <div className={styles.container}>
         <div className={styles.descriptions}>
@@ -28,7 +20,7 @@ function Categories() {
             <Link href="/categories">View All Categories</Link>
         </div>
         <div className={styles.categories}>
-          {categories.map((category,index) => <Category key={category.id} data={category}  style ={{backgroundColor:backgroundColor[index]}}/>)}   
+          {categories.map((category) => <Category key={category.id} data={category} />)}   
         </div>
     </div>
   )
