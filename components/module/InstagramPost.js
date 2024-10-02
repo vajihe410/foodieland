f31daf658b1201b754ad39aef096d1404feef0ed
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-10-02 05:56:51 +0330

    Feat & Styles:create instagramPost component with styles

--- components/module/InstagramPost.js
+++ components/module/InstagramPost.js
@@ -1,10 +1,57 @@
+import Image from "next/image"
 //Styles
 import styles from "./InstagramPost.module.css" 
+//Icons
+import More from "../icons/More"
+import Check from "../icons/Check"
+import Like from "../icons/Like"
+import Comment from "../icons/Comment"
+import Share from "../icons/Share"
+import Save from "../icons/Save"
 
 function InstagramPost({data}) {
   console.log(data)
   return (
-    <div>InstagramPost</div>
+    <div className={styles.card}>
+      <div className={styles.card_header}>
+          <div className={styles.profile} >
+            <Image className={styles.profile_image} src="/images/profile.png" alt="profile" width={800} height={800}/>
+            <div className={styles.profile_texts}>
+              <div><span className={styles.profile_name}>Foodieland.</span> <Check/></div>
+              <span className={styles.profile_location}>{data.location}</span>
+            </div>
+          </div>
+          <More className={styles.more}/>
+      </div>
+      <div className={styles.card_body}>
+        <Image className={styles.post} src={data.src} alt={data.name} width={1000} height={1000}/>
+        <span>1/3</span>
+      </div>
+      <div className={styles.card_footer}>
+        <div className={styles.interaction}>
+          <div>
+            <Like/>
+            <Comment/>
+            <Share/>
+          </div>
+          <div className={styles.pages}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <Save/>
+        </div>
+        <div className={styles.captions}>
+          <div>
+            <Image className={styles.follower} src="/images/follower.png" alt="follower" width={800} height={800}/>
+            <p>{`Liked by craig_love and ${data.likes} others`}</p>
+          </div>
+          <p><span>Foodieland</span>{data.captions}</p>
+          <span className={styles.date}>{data.publish}</span>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
