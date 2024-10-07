710a198c0bbd057557385f89629d2597cbc2a37a
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-10-07 11:28:48 +0330

    Feat: change data of instagram posts to global

---
+++ data/instagramPostData.js
@@ -0,0 +1,7 @@
+const instagramPosts = [
+    {id:1, src:"/images/instagramPost1.png", alt:"food", location:"Tokyo, Japan", caption:"The vegetables dishes need to have certain vitamin for those people", likes:"44,686", publish:"September 19" },
+    {id:2, src:"/images/instagramPost2.png", alt:"food", location:"Tokyo, Japan", caption:"Sweet food can bring someone into happiness as long as they don't eat to much", likes:"44,686", publish:"September 19"},
+    {id:3, src:"/images/instagramPost3.png", alt:"food", location:"Tokyo, Japan", caption:"What are you doing before start cooking? prepare thr tools or ingredients?", likes:"44,686", publish:"September 19"},
+    {id:4, src:"/images/instagramPost4.png", alt:"food", location:"Tokyo, Japan", caption:"Steak never be wrong, it's suitable for you who want romantic dinner", likes:"44,686", publish:"September 19"},
+]
+export default instagramPosts
