9a00e69b5b18e17882b012717d186dbd009be42a
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-10-07 10:26:32 +0330

    Feat:use global data for foods

--- components/module/AboutInstagram.js
+++ components/module/AboutInstagram.js
@@ -8,10 +8,10 @@ import Instagram from "../icons/Instagram"
 
 function AboutInstagram() {
     const instagramPosts = [
-        {id:1, src:"/images/food9.png", alt:"food", location:"Tokyo, Japan", caption:"The vegetables dishes need to have certain vitamin for those people", likes:"44,686", publish:"September 19" },
-        {id:2, src:"/images/food10.png", alt:"food", location:"Tokyo, Japan", caption:"Sweet food can bring someone into happiness as long as they don't eat to much", likes:"44,686", publish:"September 19"},
-        {id:3, src:"/images/food11.png", alt:"food", location:"Tokyo, Japan", caption:"What are you doing before start cooking? prepare thr tools or ingredients?", likes:"44,686", publish:"September 19"},
-        {id:4, src:"/images/food12.png", alt:"food", location:"Tokyo, Japan", caption:"Steak never be wrong, it's suitable for you who want romantic dinner", likes:"44,686", publish:"September 19"},
+        {id:1, src:"/images/instagramPost1.png", alt:"food", location:"Tokyo, Japan", caption:"The vegetables dishes need to have certain vitamin for those people", likes:"44,686", publish:"September 19" },
+        {id:2, src:"/images/instagramPost2.png", alt:"food", location:"Tokyo, Japan", caption:"Sweet food can bring someone into happiness as long as they don't eat to much", likes:"44,686", publish:"September 19"},
+        {id:3, src:"/images/instagramPost3.png", alt:"food", location:"Tokyo, Japan", caption:"What are you doing before start cooking? prepare thr tools or ingredients?", likes:"44,686", publish:"September 19"},
+        {id:4, src:"/images/instagramPost4.png", alt:"food", location:"Tokyo, Japan", caption:"Steak never be wrong, it's suitable for you who want romantic dinner", likes:"44,686", publish:"September 19"},
     ]
   return (
     <div className={styles.container}>
--- components/module/Recipes.js
+++ components/module/Recipes.js
@@ -3,25 +3,18 @@ import FoodCard from "./FoodCard"
 import FoodAd from "./FoodAd"
 //Styles
 import styles from "./Recipes.module.css"
+//Data 
+import foodData from "../../data/foodData"
 
 function Recipes() {
-  const cardFoods = [
-    {id:"1", name: "Big and juicy Wagyu Beef Cheeseburger", time: "30 Minutes", type: "Snack" , like: true , image:"/images/food1.png"},
-    {id:"2", name: "Fresh Lime Roasted Salmon with Ginger Sauce", time: "30 Minutes", type: "Fish" , like: false , image:"/images/food2.png"},
-    {id:"3", name: "Strawberry Oatmeal Pancake with Honey Syrup", time: "30 Minutes", type: "Breakfast" , like: false , image:"/images/food3.png"},
-    {id:"4", name: "Fresh and Healthy Mixed Mayonnaise Saled", time: "30 Minutes", type: "Healthy" , like: true , image:"/images/food4.png"},
-    {id:"5", name: "Chicken Meatballs with Cream Cheese", time: "30 Minutes", type: "Meat" , like: false , image:"/images/food5.png"},
-    {id:"6", name: "Fruity Pancake with Orange & Blueberry", time: "30 Minutes", type: "Sweet" , like: true , image:"/images/food6.png"},
-    {id:"7", name: "The Best Easy One Pot Chicken and Rice", time: "30 Minutes", type: "Snack" , like: false , image:"/images/food7.png"},
-    {id:"8", name: "the Creamiest Creamy Chicken and Bacon Pasta", time: "30 Minutes", type: "Noodles" , like: false , image:"/images/food8.png"},
-  ]
+  const cardFoods = foodData.slice(0,8)
   return (
     <div className={styles.container}>
         <h3>Simple and tasty recipes</h3>
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim</p>
         <div className={styles.cards}>
           {
-          cardFoods.map(item => <FoodCard key={item.id} data={item}/>)
+          cardFoods.map((item)=> <FoodCard key={item.id} data={item} />)
           }
         </div>
         <FoodAd/>
