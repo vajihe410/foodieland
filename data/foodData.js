675a38553ba6f9ddd87133deb1057a2a68ed62d6
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-10-07 10:24:59 +0330

    Feat:create data of foods

---
+++ data/foodData.js
@@ -0,0 +1,20 @@
+const foodData =[
+    {id:"1", name: "Big and juicy Wagyu Beef Cheeseburger", time: "30 Minutes", type: "Snack" , like: true , image:"/images/food1.png"},
+    {id:"2", name: "Fresh Lime Roasted Salmon with Ginger Sauce", time: "30 Minutes", type: "Fish" , like: false , image:"/images/food2.png"},
+    {id:"3", name: "Strawberry Oatmeal Pancake with Honey Syrup", time: "30 Minutes", type: "Breakfast" , like: false , image:"/images/food3.png"},
+    {id:"4", name: "Fresh and Healthy Mixed Mayonnaise Saled", time: "30 Minutes", type: "Healthy" , like: true , image:"/images/food4.png"},
+    {id:"5", name: "Chicken Meatballs with Cream Cheese", time: "30 Minutes", type: "Meat" , like: false , image:"/images/food5.png"},
+    {id:"6", name: "Fruity Pancake with Orange & Blueberry", time: "30 Minutes", type: "Sweet" , like: true , image:"/images/food6.png"},
+    {id:"7", name: "The Best Easy One Pot Chicken and Rice", time: "30 Minutes", type: "Snack" , like: false , image:"/images/food7.png"},
+    {id:"8", name: "the Creamiest Creamy Chicken and Bacon Pasta", time: "30 Minutes", type: "Noodles" , like: false , image:"/images/food8.png"},
+    {id:"9", name: "Mixed Tropical Fruit Salad with Superfood Boosts ", time: "30 Minutes", type: "Healthy" , like: true , image:"/images/food9.png"},
+    {id:"10", name: "Big and Juicy Wagyu Beef Cheeseburger", time: "30 Minutes", type: "Western" , like: false , image:"/images/food10.png"},
+    {id:"11", name: "Healthy Japanese Fried Rice with Asparagus", time: "30 Minutes", type: "Healthy" , like: true , image:"/images/food11.png"},
+    {id:"12", name: "Cauliflower Walnut Vegetarian Taco Meat", time: "30 Minutes", type: "Eastern" , like: false , image:"/images/food12.png"},
+    {id:"13", name: "Rainbow Chicken Salad with Almond Honey Mustard Dressing", time: "30 Minutes", type: "Healthy" , like: true , image:"/images/food13.png"},
+    {id:"14", name: "Barbeque Spicy Sandwiches with Chips ", time: "30 Minutes", type: "Snack" , like: false , image:"/images/food14.png"},
+    {id:"15", name: "Firecracker Vegan Lettuce Wraps - Spicy! ", time: "30 Minutes", type: "Seafood" , like: false , image:"/images/food15.png"},
+    {id:"16", name: "Chicken Ramen Soup with Mushroom ", time: "30 Minutes", type: "Japanese" , like: true , image:"/images/food16.png"},
+  ]
+
+export default foodData  
