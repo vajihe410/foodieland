4b7587c2a7f641c9e28ddafd530023fd4a268b0f
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-10-12 04:30:45 +0330

    Feat: seprate data of categories from component & create route of categories and category

--- components/module/Categories.js
+++ components/module/Categories.js
@@ -1,18 +1,12 @@
 import Link from 'next/link'
 //Components
 import Category from './Category'
+//Data
+import categories from '../../data/category'
 //Styles
 import styles from "./Categories.module.css"
 
 function Categories() {
-    const categories = [
-        {id:1, src:"/images/Breakfast.png", alt:"Breakfast", name:"Breakfast"},
-        {id:2, src:"/images/Vegan.png", alt:"Vegan", name:"Vegan"},
-        {id:3, src:"/images/Meat.png", alt:"Meat", name:"Meat", },
-        {id:4, src:"/images/Dessert.png", alt:"Dessert", name:"Dessert"},
-        {id:5, src:"/images/Lunch.png", alt:"Lunch", name:"Lunch"},
-        {id:6, src:"/images/Chocolate.png", alt:"Chocolate", name:"Chocolot"},
-    ]
   return (
     <div className={styles.container}>
         <div className={styles.descriptions}>
---
+++ data/category.js
@@ -0,0 +1,9 @@
+const categories = [
+    {id:1, src:"/images/Breakfast.png", alt:"Breakfast", name:"Breakfast"},
+    {id:2, src:"/images/Vegan.png", alt:"Vegan", name:"Vegan"},
+    {id:3, src:"/images/Meat.png", alt:"Meat", name:"Meat", },
+    {id:4, src:"/images/Dessert.png", alt:"Dessert", name:"Dessert"},
+    {id:5, src:"/images/Lunch.png", alt:"Lunch", name:"Lunch"},
+    {id:6, src:"/images/Chocolate.png", alt:"Chocolate", name:"Chocolot"},
+]
+export default categories
